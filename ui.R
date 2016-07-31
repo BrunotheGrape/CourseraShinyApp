228d0bd4d02ae58f61fa97fb5f26afff40fd02f3
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-31 18:46:24 -0500

    code checkboxes in user interface

--- ui.R
+++ ui.R
@@ -6,6 +6,10 @@ options(digits = 12)
 # Define UI for application that draws a histogram
 shinyUI(pageWithSidebar(
   headerPanel( "GDP correlations"),
-  sidebarPanel(),
+  sidebarPanel(
+    checkboxInput("argentina", label = "Argentina", value = TRUE),
+    checkboxInput("brazil", label = "Brazil", value = TRUE),
+    checkboxInput("chile", label = "Chile", value = TRUE)
+  ),
   mainPanel(plotOutput('plot1'))
 ))
