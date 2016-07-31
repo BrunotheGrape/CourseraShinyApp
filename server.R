5ce0ecf1fbe6629e3ee764e7551c1668202be317
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-31 13:48:44 -0500

    initial plot formatting

--- server.R
+++ server.R
@@ -4,11 +4,12 @@ library(shiny); library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-   
+  options(scipen = 999)
  GDP <- read.csv("WB_GDP.csv", header = TRUE)
    output$plot1 <- renderPlot({
     
- p <-   ggplot(GDP, aes(China, Brazil)) + xlab("China GDP")
+ p <-   ggplot(GDP, aes(China, GDP)) 
+ p <- p + xlab("China GDP in $ Millions") +ylab("Comparison GDP in $ Milliions")
  print(p)
     
   })
