7f23e097f741d1d5776698cad6c5c65550587b21
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-31 18:47:17 -0500

    server side functionality for checkboxes

--- server.R
+++ server.R
@@ -5,22 +5,38 @@ library(shiny); library(ggplot2)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   options(scipen = 999)
-  a <- c("#75AADB")
-  b <- c("#007600")
-  c <- c("#d52b1e")
-  Plt <- c(a, b, c)
   
   
   
+ 
  WBGDP <- read.csv("WB_GDP.csv", header = TRUE)
    output$plot1 <- renderPlot({
+  
+     a <- c("#E5F5F9")
+     b <- c("#E5F5F9")
+     c <- c("#E5F5F9")
     
+     
+     if (input$argentina) { 
+       a <- c("#75AADB")
+     }   
+     
+     if (input$brazil) { 
+       b <- c("#007600")
+     } 
+     
+     if (input$chile) { 
+       c <- c("#D52B1E")
+     } 
+     
+     
+     Plt <- c(a, b, c)     
  p <-   ggplot(WBGDP, aes(China, GDP, colour = Country, shape = Country, size = 4)) 
  p <- p + labs(title = "Selected Correlations", x = "China GDP in $ Millions", y = "Comparison GDP in $ Milliions")
  p <- p + geom_point() + scale_color_manual(values = Plt)
  p <- p + theme(panel.background = element_rect(fill = "#E5F5F9"))
  
- print(p)
+  print(p)
     
   })
   
