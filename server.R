c6d07a2a850c38dda709eed92eaf3afeb66407c9
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-31 15:30:30 -0500

    establishing fill

--- server.R
+++ server.R
@@ -5,11 +5,15 @@ library(shiny); library(ggplot2)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   options(scipen = 999)
- GDP <- read.csv("WB_GDP.csv", header = TRUE)
+ WBGDP <- read.csv("WB_GDP.csv", header = TRUE)
    output$plot1 <- renderPlot({
     
- p <-   ggplot(GDP, aes(China, GDP)) 
- p <- p + xlab("China GDP in $ Millions") +ylab("Comparison GDP in $ Milliions")
+ p <-   ggplot(WBGDP, aes(China, GDP, fill = Country)) 
+ p <- p + labs(title = "Selected Correlations", x = "China GDP in $ Millions", y = "Comparison GDP in $ Milliions")
+ 
+ p <- p + geom_point()
+
+ 
  print(p)
     
   })
