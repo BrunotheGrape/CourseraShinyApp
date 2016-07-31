f1cf63b57a592068e01906fcd68c3f1f64d16ba4
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-31 16:54:32 -0500

    establish check boxes

--- server.R
+++ server.R
@@ -10,10 +10,12 @@ shinyServer(function(input, output, session) {
   c <- c("#d52b1e")
   Plt <- c(a, b, c)
   
+  
+  
  WBGDP <- read.csv("WB_GDP.csv", header = TRUE)
    output$plot1 <- renderPlot({
     
- p <-   ggplot(WBGDP, aes(China, GDP, colour = Country, shape = Country)) 
+ p <-   ggplot(WBGDP, aes(China, GDP, colour = Country, shape = Country, size = 4)) 
  p <- p + labs(title = "Selected Correlations", x = "China GDP in $ Millions", y = "Comparison GDP in $ Milliions")
  p <- p + geom_point() + scale_color_manual(values = Plt)
  p <- p + theme(panel.background = element_rect(fill = "#E5F5F9"))
