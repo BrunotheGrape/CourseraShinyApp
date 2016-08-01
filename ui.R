3a764c12af07ae5170b95fd1207ff9df005179fd
Boyd Ingalls <boyd.ingalls@gmail.com>
2016-07-31 19:22:13 -0500

    title checkbox

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ options(digits = 12)
 # Define UI for application that draws a histogram
 shinyUI(pageWithSidebar(
   headerPanel( "GDP Correlations"),
-  sidebarPanel(
+  sidebarPanel( "Select GDP/China Correlation",
     checkboxInput("argentina", label = "Argentina", value = TRUE),
     checkboxInput("brazil", label = "Brazil", value = TRUE),
     checkboxInput("chile", label = "Chile", value = TRUE)
